b2481f9d674f117f4e344186d63dd98fe5b6656d
Safdar Kureishy <safdar.kureishy@gmail.com>
2017-03-06 18:42:57 -0800

    Polished score functions + added some combo functions

--- scorefunctions.py
+++ scorefunctions.py
@@ -97,18 +97,9 @@ def improved_score(game, player):
     opp_moves = len(game.get_legal_moves(game.get_opponent(player)))
     return float(own_moves - opp_moves)
 
-def more_improved_score(game, player):
-    if game.is_loser(player):
-        return float("-inf")
-    if game.is_winner(player):
-        return float("inf")
-    own_moves = game.get_legal_moves(player)
-    opp_moves = game.get_legal_moves(game.get_opponent(player))
-    if game.active_player == player:
-        return float(len(own_moves) - len(opp_moves))
-    else: # It' the opponent's turn, so revise the options a bit
-        return float(len([x for x in own_moves if x not in opp_moves]) - len(opp_moves))
-
+#################################################################
+#                    CUSTOM SCORING ATTEMPTS                    #
+#################################################################
 def custom_score(game, player):
     """Calculate the heuristic value of a game state from the point of view
     of the given player.
@@ -131,14 +122,44 @@ def custom_score(game, player):
     float
         The heuristic value of the current game state to the specified player.
     """
-    score = 0
-    score += mobility_score(game, player)
-    score += overlap_score(game, player)
-#     score += accessibility_score(game, player)
-#     score += proximity_score(game, player)
-#     score += flexibility_score(game, player)
+
+    # Strategy:
+    # - First half: Stay near center + stay away from other
+    # - Second half: Stay close + offensive + net_mobility
+    # - Third half: net_advantage
+    stage = game_stage(game)
+    if stage <= 33.0:
+        return combo_nearcenter_avoidopponent_score(game, player)
+    elif stage <= 66.0:
+        return combo_offensive_nearopponent_netmobility_score(game, player)
+    else:
+        return combo_netadvantage_nearopponent_score(game, player)
+
+def combo_nearcenter_avoidopponent_score(game, player):
+    score = 0.
+    score += accessibility_score(game, player)
+    score -= proximity_score(game, player)
     return score
-    
+
+def combo_offensive_nearopponent_netmobility_score(game, player):
+    score = 0.
+    score += offensive_score(game, player)
+    score += proximity_score(game, player)
+    return score
+
+def combo_netadvantage_nearopponent_score(game, player):
+    score = 0.
+    score += net_advantage_score(game, player)
+    score += offensive_score(game, player)
+    return score
+
+def game_stage(game):
+    total_spaces = game.width * game.height
+    remaining_spaces = game.get_blank_spaces_count()
+    assert total_spaces >= remaining_spaces
+    percent_remaining = 100 * ((total_spaces - remaining_spaces) / total_spaces)
+    return 100 - percent_remaining
+
 # Number of hops between two positions on the board
 def gethopdistance(x, y):
     dx = abs(x[0] - y[0])
@@ -154,20 +175,38 @@ def winlose_score(game, player):
         return float("inf")
     return 0
 
+def net_advantage_score(game, player):
+    score = winlose_score(game, player)
+    if -INFINITY < score < INFINITY:
+        own_moves = game.get_legal_moves(player)
+        opp_moves = game.get_legal_moves(game.get_opponent(player))
+        if game.active_player == player:
+            score += float(len(own_moves) - len(opp_moves))
+        else: # It' the opponent's turn, so revise the options a bit
+            score += float(len([x for x in own_moves if x not in opp_moves]) - len(opp_moves))
+    return score
+
 # Has more move options than opponent
-def mobility_score(game, player):
+def net_mobility_score(game, player):
     score = winlose_score(game, player)
     if -INFINITY < score < INFINITY:
         own_moves = len(game.get_legal_moves(player))
         opp_moves = len(game.get_legal_moves(game.get_opponent(player)))
-        if own_moves > opp_moves:
-            score += 1
-        elif own_moves < opp_moves:
-            score -= 1
+        if player == game.active_player:
+            if own_moves > opp_moves:
+                score += 2              # Active player so extra adv
+            elif own_moves < opp_moves:
+                score -= 1
+        else:
+            # Having fewer moves is worse if we're not the next player
+            if own_moves > opp_moves:
+                score += 1
+            elif own_moves < opp_moves: # Not active, so worse-off
+                score -= 2
     return score
     
 # Has overlap with opponent's next options
-def overlap_score(game, player):
+def offensive_score(game, player):
     score = winlose_score(game, player)
     if -INFINITY < score < INFINITY:
         own_moves = game.get_legal_moves(player)
@@ -190,10 +229,13 @@ def accessibility_score(game, player):
         delta = gethopdistance(own_location, center)
         if delta < round(center[0]/2):
             score += 1
+        elif delta > round(center[0]/2):
+            score += -1
+            
     return score
 
-# Distance from the patches of open spaces
-# def flexibility_score(game, player):
+# Distance from the patches of open spaces. Tryes to avoid the horizon problem.
+# def horizon_score(game, player):
 #     score = winlose_score(game, player)
 #     if -INFINITY < score < INFINITY:
 #         own_moves = len(game.get_legal_moves(player))
@@ -206,13 +248,13 @@ def accessibility_score(game, player):
 #         total_moves = own_moves + opp_moves
 #     return score
 
-# Proximity from each other
+# Proximity from each other: Closer = higher
 def proximity_score(game, player):
     score = winlose_score(game, player)
     if -INFINITY < score < INFINITY:
         own_location = game.get_player_location(player)
         opp_location = game.get_player_location(game.get_opponent(player))
         delta = gethopdistance(own_location, opp_location)
-        score += delta   # Since the knight moves in an L shape (so can jump 2 spaces net)
+        score -= delta   # Since the knight moves in an L shape (so can jump 2 spaces net)
     return score
 
